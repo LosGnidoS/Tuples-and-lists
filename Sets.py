4cd1ab0b82b85d57ae60aa2df2ac8f5d3033732f
Kirill Shvedov <github.shvedov@gmail.com>
2020-07-31 00:05:57 +0300

    Add files via upload

---
+++ Sets.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+#SETS
+names = {"Tien","Omega","Marion",
+"Dionna","Elise","Amparo","Janetta",
+"Leslee","Wendi","Chantell","Deanne",
+"Maire","Genaro","Merlyn","Lakeisha",
+"Bobbie","Carolin","Raelene",
+"Krystal","Felicia" }
+
+popular_names = {"Dasha", "Raelene", "Krystal",
+"Kirill","Anton","Bobbie"}
+
+#Determine common names from 2 sets
+common_names = names & popular_names 
+print(common_names)
+
+
+
